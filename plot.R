51bfa04cce8ac4577b000ba47b7c4b93efb537c1
Hagar Nabil <47554625+hagar912@users.noreply.github.com>
2019-06-27 18:06:30 +0200

    Delete plot.R

--- plot.R
+++
@@ -1,8 +0,0 @@
-setwd("I:\NTL\files\exdata_data_household_power_consumption")
-##reading data from the file
-data_full<-read.csv("household_power_consumption.txt", header=T, sep=';', na.string="?" ,nrow=2075259)
-## subset the data according to the time
-data1 <- subset(data_full, Date %in% c("1/2/2007","2/2/2007"))
-#formating the date
-data1$Date <- as.Date(data1$Date, format="%d/%m/%Y")
-##plotting the hist
